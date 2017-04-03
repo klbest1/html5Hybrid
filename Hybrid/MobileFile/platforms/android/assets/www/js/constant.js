faa09f0663fadc5ecd5a0449af8ecf4ab895ada0
lin kang <530139052@qq.com>
2017-04-03 21:43:39 +1000

    完成保险箱界面，现在打开浏览文件有问题，搞了这么久，极有可能是沙盒中的文件，用第三方并不能打开。。。。

--- Hybrid/MobileFile/platforms/android/assets/www/js/constant.js
+++ Hybrid/MobileFile/platforms/android/assets/www/js/constant.js
@@ -2,8 +2,6 @@
  * Created by linkang on 25/03/2017.
  */
 var keyEntries = "entries";
-var keyData = "data";
-var keyType = 'type';
 var keyPassWordFinishPage = "passWordBackPage";
 var fileDealType = {
     "MovingFile":0,
@@ -15,8 +13,12 @@ var keyUserPassword = "password";
 
 /***压缩文件的表属性*/
 var keyFilePath = "filePath";
+var keyFileOriginPath = "orginPath";
 var keyFileMIMEType = "fileMimieType";
 var keyFileType = "fileType";
 var keyFileImage = "fileImage";
 var keyFileName = "fileName";
 var keyID = "id";
+
+/**取保存在html中险箱数据的key*/
+var keySafeData = "keySafeData";
